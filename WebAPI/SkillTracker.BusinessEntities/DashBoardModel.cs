310c5e674c6971c10c08494f122e2d053a33269f
Sandeep.Kurup@cognizant.com <Sandeep.Kurup@cognizant.com>
2018-05-05 08:29:57 +0530

    First commit

---
+++ WebAPI/SkillTracker.BusinessEntities/DashBoardModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillTracker.BusinessEntities
+{
+    public class DashBoardModel
+    {
+        public List<GraphModel> GraphDetails { get; set; }
+        public int RegistredCandidate { get; set; }
+        public double FemaleCandidate { get; set; }
+        public double MaleCandidate { get; set; }
+        public double FresherCandidate { get; set; }
+        public int RatedCandidate { get; set; }
+        public double FemaleRatedCandidate { get; set; }
+        public double MaleRatedCandidate { get; set; }
+        public double Level1Candidate { get; set; }
+        public double Level2Candidate { get; set; }
+        public double Level3Candidate { get; set; }
+
+        public List<AssociateModel> AssociateDetails { get; set; }
+
+    }
+}
---
+++ WebAPI/SkillTracker.BusinessEntities/GraphModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillTracker.BusinessEntities
+{
+    public class GraphModel
+    {
+        public string SkillName { get; set; }
+        public double Percent { get; set; }
+        public string Color { get; set; }
+    }
+}
